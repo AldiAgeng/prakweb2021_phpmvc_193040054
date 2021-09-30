c461c0027da90ec6221b292093c8f3c5bf0e5cf2
AldiAgeng <aldiageng48@gmail.com>
2021-09-30 09:50:28 +0700

    Selesai latihan video Membuat Aplikasi MVC dengan PHP #6 Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
+<div class="container">
   <h1>About Me</h1>
-  <p>Hallo nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+  <img class="rounded-circle" src="<?= BASEURL; ?>/img/gambar.jpg" alt="Aldi Ageng">
+  <p class="mt-3">Hallo nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
