2fbfaaec7fb6f27077f1f0d16ead5bbb40973414
AldiAgeng <aldiageng48@gmail.com>
2021-09-30 07:28:21 +0700

    Selesai latihan video Membuat Aplikasi MVC dengan PHP #3 Routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,19 @@
 class App{
   public function __construct()
   {
-    echo "OK!";
+    $url = $this->parseURL();
+    var_dump($url);
+  }
+
+  public function parseURL(){
+    if(isset($_GET['url'])){
+      // Menghapus karakter /
+      $url = rtrim($_GET['url'], '/');
+      // Membersihkan dari karakter aneh
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      // pecah url dengan delimiter /
+      $url = explode('/', $url);
+      return $url;
+    }
   }
 }
